ccfa6d528daf480b02be67336f2e7772053eba96
Catherine <x.katarzyna.ciesielska.x@gmail.com>
2025-06-19 21:41:29 +0200

    Question model update

--- Models/Question.cs
+++ Models/Question.cs
@@ -6,6 +6,7 @@
         public required string Text { get; set; }
 
         //Options
+        public required List<Option> Options { get; set; }
         public required Guid CorrectOption { get; set; }
     }
 }
